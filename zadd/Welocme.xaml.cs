354bc632a94c77223ff7d22a245c7d32b3992c53
KeYnSaINe <maksimzuev300@gmail.com>
2026-06-15 22:02:11 +0500

    first commit

---
+++ zadd/Welocme.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace zadd
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Welcome : ContentPage
+    {
+        public Welcome()
+        {
+            InitializeComponent();
+        }
+      private void Validation()
+        {
+            if (txtName.Text == null || txtPassword.Text == null) return;
+        }
+    }
+}
